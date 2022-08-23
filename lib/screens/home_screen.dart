139610d54c3c6c71a2364ec5f555a7be9e9bade3
Angger Raka Sanjaya <anggerrakasanjayapamungkas@gmail.com>
2022-08-23 14:04:41 +0700

    commit 1.1.1

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,7 +4,7 @@ import 'package:test_chatting/pages/message_page.dart';
 import 'package:test_chatting/pages/notifications_page.dart';
 import 'package:test_chatting/pages/profile_pages.dart';
 import 'package:flutter/material.dart';
-
+import 'package:test_chatting/theme.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -14,6 +14,9 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final ValueNotifier<int> pageIndex = ValueNotifier(0);
+  final ValueNotifier<String> title = ValueNotifier("Messages");
+
   final pages = const [
     MessagePage(),
     NotificationPage(),
@@ -21,31 +24,75 @@ class _HomeScreenState extends State<HomeScreen> {
     ContactsPage(),
     ProfilePage()
   ];
-  var index = 0;
+
+  final pageTitles = const [
+    'Message',
+    'Notification',
+    'Call',
+    'Contacts',
+    'Profile'
+  ];
+
+  void _onNavigationItemSelected(index) {
+    title.value = pageTitles[index];
+    pageIndex.value = index;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: pages[index],
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          title: ValueListenableBuilder(
+            valueListenable: title,
+            builder: (BuildContext context, String value, _) {
+              return Text(
+                value,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              );
+            },
+          ),
+        ),
+        body: ValueListenableBuilder(
+          valueListenable: pageIndex,
+          builder: (BuildContext context, int value, _) {
+            return pages[value];
+          },
+        ),
         bottomNavigationBar: SafeArea(
           child: _BottomNavigationBar(
-            onItemsSelected: (i) {
-              setState(() {
-                index = i;
-              });
-            },
+            onItemsSelected: _onNavigationItemSelected,
           ),
         ));
   }
 }
 
-class _BottomNavigationBar extends StatelessWidget {
+class _BottomNavigationBar extends StatefulWidget {
   const _BottomNavigationBar({
     Key? key,
     required this.onItemsSelected,
   }) : super(key: key);
 
   final ValueChanged<int> onItemsSelected;
+
+  @override
+  State<_BottomNavigationBar> createState() => _BottomNavigationBarState();
+}
+
+class _BottomNavigationBarState extends State<_BottomNavigationBar> {
+  var selectedIndex = 0;
+
+  void handleItemSelected(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+    widget.onItemsSelected(index);
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -60,31 +107,36 @@ class _BottomNavigationBar extends StatelessWidget {
               index: 0,
               label: "Home",
               icon: Icons.home,
-              onTap: onItemsSelected,
+              isSelected: (selectedIndex == 0),
+              onTap: handleItemSelected,
             ),
             _NavigatiionBarItem(
               index: 1,
               label: "Messaging",
               icon: Icons.message,
-              onTap: onItemsSelected,
+              isSelected: (selectedIndex == 1),
+              onTap: handleItemSelected,
             ),
             _NavigatiionBarItem(
               index: 2,
               label: "Order",
               icon: Icons.shop,
-              onTap: onItemsSelected,
+              isSelected: (selectedIndex == 2),
+              onTap: handleItemSelected,
             ),
             _NavigatiionBarItem(
               index: 3,
               label: "Delivery",
               icon: Icons.delivery_dining,
-              onTap: onItemsSelected,
+              isSelected: (selectedIndex == 3),
+              onTap: handleItemSelected,
             ),
             _NavigatiionBarItem(
               index: 4,
               label: "Profile",
               icon: Icons.person,
-              onTap: onItemsSelected,
+              isSelected: (selectedIndex == 4),
+              onTap: handleItemSelected,
             ),
           ],
         ),
@@ -99,10 +151,12 @@ class _NavigatiionBarItem extends StatelessWidget {
     required this.index,
     required this.label,
     required this.icon,
+    this.isSelected = false,
     required this.onTap,
   }) : super(key: key);
 
   final ValueChanged<int> onTap;
+  final bool isSelected;
   final int index;
   final String label;
   final IconData icon;
@@ -110,6 +164,7 @@ class _NavigatiionBarItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
+      behavior: HitTestBehavior.opaque,
       onTap: () {
         onTap(index);
       },
@@ -121,13 +176,19 @@ class _NavigatiionBarItem extends StatelessWidget {
             Icon(
               icon,
               size: 25,
+              color: isSelected ? AppColors.secondary : null,
             ),
             const SizedBox(
               height: 5,
             ),
             Text(
               label,
-              style: TextStyle(fontSize: 13),
+              style: isSelected
+                  ? TextStyle(
+                      fontSize: 13,
+                      fontWeight: FontWeight.bold,
+                      color: AppColors.secondary)
+                  : TextStyle(fontSize: 13),
             )
           ],
         ),
