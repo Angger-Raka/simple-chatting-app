4736e128f2a886ea1f7a50e9cbe5147494a1f563
Angger Raka Sanjaya <anggerrakasanjayapamungkas@gmail.com>
2022-08-26 11:17:09 +0700

    commit 1.1.3

--- lib/pages/message_page.dart
+++ lib/pages/message_page.dart
@@ -1,12 +1,144 @@
+import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
+import 'package:jiffy/jiffy.dart';
+import 'package:test_chatting/helper.dart';
+import 'package:test_chatting/models/models.dart';
+import 'package:test_chatting/theme.dart';
+import 'package:test_chatting/widgets/avatar.dart';
 
 class MessagePage extends StatelessWidget {
   const MessagePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('MessagePage'),
+    return CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: _Stories(),
+        ),
+        SliverList(
+          delegate: SliverChildBuilderDelegate(_delegate),
+        )
+      ],
+    );
+  }
+
+  Widget _delegate(BuildContext context, int index) {
+    final Faker faker = Faker();
+    final date = Helpers.randomDate();
+    return _MessageTitle(
+        messageData: MessageData(
+            senderName: faker.person.name(),
+            message: faker.lorem.sentence(),
+            messageDate: date,
+            dateMessage: Jiffy(date).fromNow(),
+            profilePicture: Helpers.randomPictureUrl()));
+  }
+}
+
+class _MessageTitle extends StatelessWidget {
+  const _MessageTitle({Key? key, required this.messageData}) : super(key: key);
+
+  final MessageData messageData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Padding(
+            padding: EdgeInsets.all(8),
+            child: Avatar.medium(imageUrl: messageData.profilePicture)),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+            Text(messageData.senderName),
+            Text(messageData.message)
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class _Stories extends StatelessWidget {
+  const _Stories({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 0,
+      child: SizedBox(
+        height: 134,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(left: 16, top: 8, bottom: 16),
+              child: Text(
+                'Stories',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15,
+                    color: AppColors.textFaded),
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (BuildContext context, int index) {
+                  final faker = Faker();
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SizedBox(
+                      width: 60,
+                      child: _StoryCard(
+                          storyData: StoryData(
+                        name: faker.person.name(),
+                        url: Helpers.randomPictureUrl(),
+                      )),
+                    ),
+                  );
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _StoryCard extends StatelessWidget {
+  const _StoryCard({
+    Key? key,
+    required this.storyData,
+  }) : super(key: key);
+
+  final StoryData storyData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Avatar.medium(imageUrl: Helpers.randomPictureUrl()),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 16),
+            child: Text(
+              storyData.name,
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(
+                  fontSize: 11,
+                  letterSpacing: 0.3,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
